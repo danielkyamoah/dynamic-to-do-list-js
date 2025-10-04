54404ad8e50e029a9947ee6e60482be936053035
Daniel Kofi Yamoah <dnnkofi@gmail.com>
2025-10-04 22:33:30 +0000

    modified script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function loadTasks() {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  storedTasks.forEach(taskText => addTask(taskText, false)); 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+});
 
